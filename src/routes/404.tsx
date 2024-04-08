81a876165d14709656e97f6a1a2467f10b89d0e3
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2024-04-08 00:25:14 -0300

    feat: 404 root layout

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,5 +1,62 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useSignal, useTask$ } from '@builder.io/qwik'
+
+import fs from 'fs'
+import path from 'path'
 
 export default component$(() => {
-	return <h1>404: Not Found</h1>
+	const routes = useSignal<string[]>([])
+
+	useTask$(() => {
+		const root = process.cwd()
+		const dirPath = path.join(root, 'src/routes')
+		const files: string[] = []
+		files.push('/')
+
+		fs.readdirSync(dirPath).forEach(file => {
+			const absolute = path.join(dirPath, file)
+			const isDir = fs.statSync(absolute).isDirectory()
+			const route = absolute.replace(`${root}/src/routes/`, '')
+			if (isDir) return files.push(route)
+		})
+
+		routes.value = files
+	})
+
+	return (
+		<>
+			<header class='grid place-items-center p-24'>
+				<h1 class='text-9xl font-thin'>404</h1>
+				<h2 class='text-5xl'>Page Not Found</h2>
+			</header>
+			<main class='mx-auto flex h-fit flex-col text-lg'>
+				<h3 class='mb-24'>Please go back to one of the following routes:</h3>
+				<hr class='text-surface-900/50' />
+				<nav class='mb-auto grid gap-4 text-center'>
+					{routes.value.map((route, index) => (
+						<a class='py-4 capitalize' href={route} key={index}>
+							{route === '/' ? 'home' : route}
+						</a>
+					))}
+				</nav>
+				<hr class='text-surface-900/50' />
+			</main>
+			<footer>
+				<small class='mb-4 inline-block w-full text-center text-sm font-thin'>
+					Made with 💖 by Johann using{' '}
+					<a class='font-bold' href='https://qwik.builder.io/' target='_blank'>
+						Qwik
+					</a>{' '}
+					based on the design by{' '}
+					<a
+						class='font-bold'
+						href='https://brittanychiang.com/#projects'
+						target='_blank'
+					>
+						Brittany Chiang
+					</a>
+					.
+				</small>
+			</footer>
+		</>
+	)
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,9 @@ export default {
 			transitionProperty: {
 				width: 'width',
 			},
+			backdropBlur: {
+				'4xl': '96px',
+			},
 		},
 	},
 	plugins: [scrollbar],
